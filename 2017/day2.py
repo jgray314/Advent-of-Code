d1216e8541066aefad4dbc7eff4d0b2defa550eb
Jess Gray <43485987+jgray314@users.noreply.github.com>
2018-12-02 15:55:34 -0800

    2017 day 2 part 2

--- 2017/day2.py
+++ 2017/day2.py
@@ -10,14 +10,13 @@ def read():
 def max_diff(ai):
 	if len(ai) == 0:
 		return 0;
-	min = int(ai[0])
-	max = int(ai[0])
+	min = ai[0]
+	max = ai[0]
 	for v in ai:
-		iv = int(v)
-		if iv < min:
-			min = iv
-		if iv > max:
-			max = iv
+		if v < min:
+			min = v
+		if v > max:
+			max = v
 	return max - min
 
 def part1(input):
@@ -26,6 +25,22 @@ def part1(input):
 		checksum += max_diff(ai)
 	return checksum
 
+def even_div_result(ai):
+	for v1 in ai:
+		for v2 in ai:
+			if v1 == v2 or (v1 % v2 != 0 and v2 % v1 != 0):
+				continue
+			if v1 > v2:
+				return int(v1 / v2)
+			else:
+				return int(v2 / v1)
+	return 0
+	
+def part2(input):
+	sum = 0
+	for ai in input:
+		sum += even_div_result(ai)
+	return sum
 input = read()
-print("Part 1: {}".format(part1(input)))  # 21845 
-#print("Part 2: {}".format(part2(input)))
+print("Part 1: {}".format(part1(input)))  # 21845
+print("Part 2: {}".format(part2(input)))  # 191
