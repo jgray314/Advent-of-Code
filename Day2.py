4b5cc23158b8137ad7c0e6ba75a58661b1b8f94a
Jess Gray <43485987+jgray314@users.noreply.github.com>
2018-12-01 23:45:15 -0800

    Day 2 Part 2

--- Day2.py
+++ Day2.py
@@ -29,6 +29,26 @@ def part1(input):
 			cnt3 += 1
 	return cnt2 * cnt3
 
+def char_diff_idxs(s1, s2):
+	c_diff = []
+	for i in range(len(s1)):
+		if s1[i] != s2[i]:
+			c_diff.append(i)
+	return c_diff
+
+def part2(input):
+	print("Starting part2...")
+	for l1 in input:
+		for l2 in input:
+			if l1 == l2:
+				continue
+			cdi = char_diff_idxs(l1, l2)
+			if len(cdi) == 1:
+				idx = cdi[0]
+				print("{} vs {} at idx {}".format(l1, l2, idx))
+				return l1[:idx] + l1[(idx + 1):]
+
 print("Reading input...")
 input = read()
 print("Part 1 result: {}".format(part1(input)))  # 6642
+print("Part 2 result: {}".format(part2(input)))  # cvqlbidheyujgtrswxmckqnap
