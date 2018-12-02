5f4ec246990d2b8a936918a0f1a9e3437bb09ba7
Jess Gray <43485987+jgray314@users.noreply.github.com>
2018-12-02 15:19:58 -0800

    2017 day 1 part 2

--- 2017/day1.py
+++ 2017/day1.py
@@ -7,12 +7,20 @@ def read():
 		input.append(int(c))
 	return input
 
-def part1(input):
+def compare_idx_rot(input, diff):
 	sum = 0
 	for i in range(len(input)):
-		if input[i] == input[i-1]:
+		if input[i] == input[i+diff]:
 			sum += input[i]
 	return sum
 
+def part1(input):
+	return compare_idx_rot(input, -1)
+
+def part2(input):
+	rot = int(len(input) / -2)
+	return compare_idx_rot(input, rot)
+	
 input = read()
-print("Part 1: {}".format(part1(input)))  # 1175
+print("Part 1: {}".format(part1(input)))
+print("Part 2: {}".format(part2(input)))
