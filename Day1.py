99faf66392f7758aaa782a0c0064de6e158e221b
Jess Gray <43485987+jgray314@users.noreply.github.com>
2018-12-01 22:53:38 -0800

    Part 2

--- Day1.py
+++ Day1.py
@@ -1,9 +1,31 @@
 import sys
 
-def part1():
-	sum = 0
+def read():
+	input = []
 	for line in sys.stdin:
-		sum += int(line)
-	print(sum)
+		i = int(line)
+		input.append(i)
+	print("Read {} entries".format(len(input)))
+	return input
 
-part1() #585
+def part1(input):
+	sum = 0
+	for n in input:
+		sum += n
+	print("Part 1 answer: {}".format(sum))
+
+def part2(input):
+	seen = set({})
+	val = 0
+	for i in range (1,1000):
+		for n in input:
+			seen.add(val)
+			val += n
+			if val in seen:
+				print("Part 2 answer: {}".format(val))
+				return
+		print("Loop {} of input complete.".format(i))
+
+input  = read()
+part1(input) # 585
+part2(input) # 83173
