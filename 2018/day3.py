fc56aad9fe7283229a6e4a651c7c449828402dbf
Jess Gray <43485987+jgray314@users.noreply.github.com>
2018-12-02 21:41:31 -0800

    2018 day 3 part 2

--- 2018/day3.py
+++ 2018/day3.py
@@ -21,18 +21,34 @@ def read():
 def idx(x, y):
 	return x * 1000 + y
 
-def part1(input):
+def get_fabric_overlay(input):
 	fabric = defaultdict(list)
 	for k, v in input.items():
 		for dx in range(v['width']):
 			for dy in range(v['height']):
 				fabric[idx(v['x']+dx, v['y']+dy)].append(k)
-	#print(fabric)
+	return fabric
+
+def part1(fabric):
 	sum = 0
 	for k, v in fabric.items():
 		if len(v) > 1:
 			sum += 1
 	return sum
 
+def part2(fabric):
+	max_overlap = defaultdict(int)
+	for k, v in fabric.items():
+		overlap = len(v)
+		for item in v:
+			if max_overlap[item] < overlap:
+				max_overlap[item] = overlap
+	for k, v in max_overlap.items():
+		if v == 1:
+			return k
+	return 0
+
 input = read()
-print("Part 1: {}".format(part1(input)))
+fabric = get_fabric_overlay(input)
+print("Part 1: {}".format(part1(fabric)))  # 103806
+print("Part 2: {}".format(part2(fabric)))  # 625
