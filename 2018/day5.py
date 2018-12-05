005972a6d2ee3d38a0c2587472c520adde90582c
Jess Gray <43485987+jgray314@users.noreply.github.com>
2018-12-04 22:20:56 -0800

    Part 2 but timing out
    if running more than one letter at a time.

--- 2018/day5.py
+++ 2018/day5.py
@@ -2,6 +2,7 @@ import sys
 from collections import defaultdict
 
 CAP_DIFF = 32
+LETTERS = "abcdefghijklmnopqrstuvwxyz"
 
 def read():
 	return sys.stdin.readline()
@@ -23,18 +24,43 @@ def react(s):
 	for i in range(len(s)):
 		if i not in react_idx:
 			new_s += s[i]
-	print("Reaction removed {} items. New string length: ".format(len(react_idx)), len(new_s))
+	#print("Reaction removed {} items. New string length: ".format(len(react_idx)), new_s)
 	return new_s
 
-def part1(input):
+def fully_react(s):
 	prev = ""
-	curr = input
+	curr = s
 	while curr != prev:
 		prev = curr
 		curr = react(prev)
 	return curr
+
+def part1(input):
+	return len(fully_react(input))
+
+def letter_strip(s, rm):
+	new_s = ""
+	for c in s:
+		if c != rm.lower() and c != rm.upper():
+			new_s += c
+	return new_s
+
+
+def part2(input):
+	letter_reacts = {}
+	for c in LETTERS:
+		c_init = letter_strip(input, c)
+		letter_reacts[c] = len(fully_react(c_init))
+	print(letter_reacts)
+	min = len(input)
+	for k, v in letter_reacts.items():
+		if v < min:
+			min = v
+	return min
+
 	
 print("Reading...")
 input = read()
 print("Initial length: ", len(input))
-print("Part 1: ", part1(input))  # 10766
+#print("Part 1: ", part1(input))  # 10766
+print("Part 2: ", part2(input))
